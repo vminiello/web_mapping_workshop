8d9ba13cdb3c7a7911e7c3019ca45e978a8e0711
vminiello <vminiello@gmail.com>
2015-03-31 20:33:51 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -11,7 +11,7 @@ var map = L.mapbox.map('map', mapId);
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
 
-var dataFileToAdd = 'data/parks.geojson';
+var dataFileToAdd = 'data/map1.geojson';
 
 var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map);
 
